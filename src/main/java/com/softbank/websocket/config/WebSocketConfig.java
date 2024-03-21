6cd06f9fafdd03dad38a8eaf8c3fc627d805ddad
Minh Nguyen <minhnguyenngoc2809@gmail.com>
2024-03-21 17:14:08 +0700

    [chatApp] Update Broker

--- src/main/java/com/softbank/websocket/config/WebSocketConfig.java
+++ src/main/java/com/softbank/websocket/config/WebSocketConfig.java
@@ -17,11 +17,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.setApplicationDestinationPrefixes("/app");
     registry.enableSimpleBroker("/topic");
-
-//    registry.enableStompBrokerRelay("/topic")
-//        .setRelayHost("localhost")
-//        .setRelayPort(61613)
-//        .setClientLogin("guest")
-//        .setClientPasscode("guest");
   }
 }
