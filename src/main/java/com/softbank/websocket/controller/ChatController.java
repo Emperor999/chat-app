dcdc51f2a3574838ec72439d8710b9204e227bcf
Minh Nguyen <minhnguyenngoc2809@gmail.com>
2024-03-21 18:32:47 +0700

    [chatApp] Cross Origin

--- src/main/java/com/softbank/websocket/controller/ChatController.java
+++ src/main/java/com/softbank/websocket/controller/ChatController.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Map;
 import java.util.Objects;
@@ -15,12 +16,14 @@ public class ChatController {
 
   @MessageMapping("/chat.sendMessage")
   @SendTo("/topic/public")
+  @CrossOrigin
   public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
     return chatMessage;
   }
 
   @MessageMapping("/chat.addUser")
   @SendTo("/topic/public")
+  @CrossOrigin
   public ChatMessage addUser(@Payload ChatMessage chatMessage,
                              SimpMessageHeaderAccessor headerAccessor) {
     Map<String, Object> sessionAttributes = Objects.requireNonNull(headerAccessor.getSessionAttributes());
